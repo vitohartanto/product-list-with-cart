e2375051542a644814bbd0b48a9de6f03922457b
Vito <vitohartanto@mail.ugm.ac.id>
2024-08-07 06:40:03 +0700

    feat: beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div>Halo Gais</div>;
+  return (
+    <div className="bg-[#fcf8f5] h-screen">
+      <h1 className="text-[#270f08] text-4xl font-bold">Desserts</h1>
+    </div>
+  );
 }
 
 export default App;
