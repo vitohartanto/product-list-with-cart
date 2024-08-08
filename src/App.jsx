0960a129e1fc8e860238be54e8a5a474581638d1
Vito <vitohartanto@mail.ugm.ac.id>
2024-08-08 15:13:01 +0700

    feat: add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Lists from './components/Lists';
+import './App.css';
 
 function App() {
   return (
-    <div className="bg-[#fcf8f5] h-screen">
-      <h1 className="text-[#270f08] text-4xl font-bold">Desserts</h1>
+    <div className="bg-[#fcf8f5] h-screen pt-4">
+      <h1 className="text-[#270f08] text-4xl font-bold ml-4 mb-4">Desserts</h1>
+      <Lists></Lists>
     </div>
   );
 }
--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import WaffleImage from '../assets/images/image-waffle-mobile.jpg';
 
 const Lists = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="px-4">
+      <div className="flex-col grid">
+        <div className="relative">
+          <img className="rounded-xl" src={WaffleImage} alt="Waffle" />
+          <div className="flex py-4 px-8 bg-white rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21"
+              height="20"
+              fill="none"
+              viewBox="0 0 21 20"
+              className="mr-4"
+            >
+              <g fill="#C73B0F" clipPath="url(#a)">
+                <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z" />
+                <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z" />
+              </g>
+              <defs>
+                <clipPath id="a">
+                  <path fill="#fff" d="M.333 0h20v20h-20z" />
+                </clipPath>
+              </defs>
+            </svg>
+            <p className="">Add to Cart</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white">Testing</div>
+    </div>
   );
 };
 
