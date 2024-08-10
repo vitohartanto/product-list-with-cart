1cf56d7eb7aa6fa98568fc444a735f1ef9d29cc8
Vito <vitohartanto@mail.ugm.ac.id>
2024-08-10 08:43:10 +0700

    feat: add Cart Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import Lists from './components/Lists';
+import Cart from './components/Cart';
 import './App.css';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
     <div className="bg-[#fcf8f5] h-screen pt-4">
       <h1 className="text-[#270f08] text-4xl font-bold ml-4 mb-4">Desserts</h1>
       <Lists></Lists>
+      <Cart></Cart>
     </div>
   );
 }
