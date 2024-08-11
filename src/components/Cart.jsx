0b10a3ea3548563027431054383d7bbfc95a8599
Vito <vitohartanto@mail.ugm.ac.id>
2024-08-11 22:24:48 +0700

    feat: cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,51 @@
+import { current } from '@reduxjs/toolkit';
 import React from 'react';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import emptyCart from '../assets/images/illustration-empty-cart.svg';
 
 const Cart = () => {
   const { items } = useSelector((state) => state.cart);
   const [popUp, setPopUp] = useState(false);
   const dispatch = useDispatch();
+  const filterCount = items.filter((item) => item.count > 0);
+  const allPriceTotal = filterCount.reduce(
+    (accumulator, currentValue) =>
+      (accumulator += currentValue.price * currentValue.count),
+    0
+  );
 
-  return <div>{}</div>;
+  const totalCount = filterCount.reduce(
+    (accumulator, currentValue) => (accumulator += currentValue.count),
+    0
+  );
+
+  const removeCartHandler = (id) => {
+    dispatch(removeCart(id));
+  };
+
+  const confirmOrderHandler = () => {
+    return setPopUp((prev) => !prev);
+  };
+
+  return (
+    <div>
+      {filterCount && totalCount > 0 ? (
+        <div>
+          <h2>Your Cart ({totalCount})</h2>
+          {filterCount.map((item) => (
+            <div key={item.id}></div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>Your Cart (0)</h2>
+          <img src={emptyCart} alt="empty-cart" />
+          <p>Your added items will appear here</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
